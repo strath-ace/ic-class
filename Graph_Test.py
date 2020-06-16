f4232c05154b568d221692530aab51fe9181d954
Framarc93 <francesco.marchetti@strath.ac.uk>
2020-06-16 22:29:20 +0200

    First step based on tutorial code

---
+++ Graph_Test.py
@@ -0,0 +1,328 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import networkx as nx
+import plotly.graph_objs as go
+
+import pandas as pd
+from colour import Color
+from datetime import datetime
+from textwrap import dedent as d
+import json
+
+# import the css template, and pass the css template into dash
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app.title = "Intelligent Control Network"
+
+YEAR=[1990, 2020]
+ID="IC001"
+
+##############################################################################################################################################################
+def network_graph(yearRange, AccountToSearch):
+
+    #edge1 = pd.read_csv('edge1.csv')
+    node1 = pd.read_csv('IC_Taxonomy.csv')
+
+    # filter the record by datetime, to enable interactive control through the input box
+    #edge1['Datetime'] = "" # add empty Datetime column to edge1 dataframe
+    accountSet=set() # contain unique account
+    for index in range(0,len(node1)):
+        #edge1['Datetime'][index] = datetime.strptime(edge1['Date'][index], '%d/%m/%Y')
+        if node1['Year'][index]<yearRange[0] or node1['Year'][index]>yearRange[1]:
+            node1.drop(axis=0, index=index, inplace=True)
+            continue
+        accountSet.add(node1['ID'][index])
+        accountSet.add(node1['Title'][index])
+        accountSet.add(node1['Goal Knowledge'][index])
+
+    # to define the centric point of the networkx layout
+    shells=[]
+    shell1=[]
+    shell1.append(AccountToSearch)
+    shells.append(shell1)
+    shell2=[]
+    for ele in accountSet:
+        if ele!=AccountToSearch:
+            shell2.append(ele)
+    shells.append(shell2)
+
+
+    G = nx.from_pandas_edgelist(node1, 'ID', 'ID', edge_attr=True)
+    nx.set_node_attributes(G, node1.set_index('ID')['Title'].to_dict(), 'Title')
+    nx.set_node_attributes(G, node1.set_index('ID')['Year'].to_dict(), 'Year')
+    nx.set_node_attributes(G, node1.set_index('ID')['Goal Knowledge'].to_dict(), 'Goal Knowledge')
+    # pos = nx.layout.spring_layout(G)
+    # pos = nx.layout.circular_layout(G)
+    # nx.layout.shell_layout only works for more than 3 nodes
+    if len(shell2)>1:
+        pos = nx.drawing.layout.shell_layout(G, shells)
+    else:
+        pos = nx.drawing.layout.spring_layout(G)
+    for node in G.nodes:
+        G.nodes[node]['pos'] = list(pos[node])
+
+
+    if len(shell2)==0:
+        traceRecode = []  # contains edge_trace, node_trace, middle_node_trace
+
+        node_trace = go.Scatter(x=tuple([1]), y=tuple([1]), text=tuple([str(AccountToSearch)]), textposition="bottom center",
+                                mode='markers+text',
+                                marker={'size': 50, 'color': 'LightSkyBlue'})
+        traceRecode.append(node_trace)
+
+        node_trace1 = go.Scatter(x=tuple([1]), y=tuple([1]),
+                                mode='markers',
+                                marker={'size': 50, 'color': 'LightSkyBlue'},
+                                opacity=0)
+        traceRecode.append(node_trace1)
+
+        figure = {
+            "data": traceRecode,
+            "layout": go.Layout(title='Interactive Transaction Visualization', showlegend=False,
+                                margin={'b': 40, 'l': 40, 'r': 40, 't': 40},
+                                xaxis={'showgrid': False, 'zeroline': False, 'showticklabels': False},
+                                yaxis={'showgrid': False, 'zeroline': False, 'showticklabels': False},
+                                height=600
+                                )}
+        return figure
+
+
+    traceRecode = []  # contains edge_trace, node_trace, middle_node_trace
+    ############################################################################################################################################################
+    colors = list(Color('lightcoral').range_to(Color('darkred'), len(G.edges())))
+    colors = ['rgb' + str(x.rgb) for x in colors]
+
+    index = 0
+    for edge in G.edges:
+        x0, y0 = G.nodes[edge[0]]['pos']
+        x1, y1 = G.nodes[edge[1]]['pos']
+        #weight = float(G.edges[edge]['TransactionAmt']) / max(edge1['TransactionAmt']) * 10
+        trace = go.Scatter(x=tuple([x0, x1, None]), y=tuple([y0, y1, None]),
+                           mode='lines',
+                           #line={'width': weight},
+                           marker=dict(color=colors[index]),
+                           line_shape='spline',
+                           opacity=1)
+        traceRecode.append(trace)
+        index = index + 1
+    ###############################################################################################################################################################
+    node_trace = go.Scatter(x=[], y=[], hovertext=[], text=[], mode='markers+text', textposition="bottom center",
+                            hoverinfo="text", marker={'size': 50, 'color': 'LightSkyBlue'})
+
+    index = 0
+    for node in G.nodes():
+        x, y = G.nodes[node]['pos']
+        hovertext = "Title: " + str(G.nodes[node]['Title']) + "<br>" + "Goal Knowledge: " + str(
+            G.nodes[node]['Goal Knowledge'])
+        text = node1['ID'][index]
+        node_trace['x'] += tuple([x])
+        node_trace['y'] += tuple([y])
+        node_trace['hovertext'] += tuple([hovertext])
+        node_trace['text'] += tuple([text])
+        index = index + 1
+
+    traceRecode.append(node_trace)
+    ################################################################################################################################################################
+    middle_hover_trace = go.Scatter(x=[], y=[], hovertext=[], mode='markers', hoverinfo="text",
+                                    marker={'size': 20, 'color': 'LightSkyBlue'},
+                                    opacity=0)
+
+    index = 0
+    for edge in G.edges:
+        x0, y0 = G.nodes[edge[0]]['pos']
+        x1, y1 = G.nodes[edge[1]]['pos']
+        hovertext = "From: " + str(G.edges[edge]['ID']) + "<br>" + "To: " + str(
+            G.edges[edge]['ID']) + "<br>" + "Goal Knowledge: " + str(
+            G.edges[edge]['Goal Knowledge']) + "<br>" + "Year: " + str(G.edges[edge]['Year'])
+        middle_hover_trace['x'] += tuple([(x0 + x1) / 2])
+        middle_hover_trace['y'] += tuple([(y0 + y1) / 2])
+        middle_hover_trace['hovertext'] += tuple([hovertext])
+        index = index + 1
+
+    traceRecode.append(middle_hover_trace)
+    #################################################################################################################################################################
+    figure = {
+        "data": traceRecode,
+        "layout": go.Layout(title='Interactive Transaction Visualization', showlegend=False, hovermode='closest',
+                            margin={'b': 40, 'l': 40, 'r': 40, 't': 40},
+                            xaxis={'showgrid': False, 'zeroline': False, 'showticklabels': False},
+                            yaxis={'showgrid': False, 'zeroline': False, 'showticklabels': False},
+                            height=600,
+                            clickmode='event+select',
+                            annotations=[
+                                dict(
+                                    ax=(G.nodes[edge[0]]['pos'][0] + G.nodes[edge[1]]['pos'][0]) / 2,
+                                    ay=(G.nodes[edge[0]]['pos'][1] + G.nodes[edge[1]]['pos'][1]) / 2, axref='x', ayref='y',
+                                    x=(G.nodes[edge[1]]['pos'][0] * 3 + G.nodes[edge[0]]['pos'][0]) / 4,
+                                    y=(G.nodes[edge[1]]['pos'][1] * 3 + G.nodes[edge[0]]['pos'][1]) / 4, xref='x', yref='y',
+                                    showarrow=True,
+                                    arrowhead=3,
+                                    arrowsize=4,
+                                    arrowwidth=1,
+                                    opacity=1
+                                ) for edge in G.edges]
+                            )}
+    return figure
+######################################################################################################################################################################
+# styles: for right side hover/click component
+styles = {
+    'pre': {
+        'border': 'thin lightgrey solid',
+        'overflowX': 'scroll'
+    }
+}
+
+app.layout = html.Div([
+    #########################Title
+    html.Div([html.H1("Intelligent Control Network Graph")],
+             className="row",
+             style={'textAlign': "center"}),
+    #############################################################################################define the row
+    html.Div(
+        className="row",
+        children=[
+            ##############################################left side two input components
+            html.Div(
+                className="two columns",
+                children=[
+                    dcc.Markdown(d("""
+                            **Time Range To Visualize**
+
+                            Slide the bar to define year range.
+                            """)),
+                    html.Div(
+                        className="twelve columns",
+                        children=[
+                            dcc.RangeSlider(
+                                id='my-range-slider',
+                                min=1992,
+                                max=2020,
+                                step=1,
+                                value=[1990, 2020],
+                                marks={
+                                    1990: {'label': '1990'},
+                                    1991: {'label': '1991'},
+                                    1992: {'label': '1992'},
+                                    1993: {'label': '1993'},
+                                    1994: {'label': '1994'},
+                                    1995: {'label': '1995'},
+                                    1996: {'label': '1996'},
+                                    1997: {'label': '1997'},
+                                    1998: {'label': '1998'},
+                                    1999: {'label': '1999'},
+                                    2000: {'label': '2000'},
+                                    2001: {'label': '2001'},
+                                    2002: {'label': '2002'},
+                                    2003: {'label': '2003'},
+                                    2004: {'label': '2004'},
+                                    2005: {'label': '2005'},
+                                    2006: {'label': '2006'},
+                                    2007: {'label': '2007'},
+                                    2008: {'label': '2008'},
+                                    2009: {'label': '2009'},
+                                    2010: {'label': '2010'},
+                                    2011: {'label': '2011'},
+                                    2012: {'label': '2012'},
+                                    2013: {'label': '2013'},
+                                    2014: {'label': '2014'},
+                                    2015: {'label': '2015'},
+                                    2016: {'label': '2016'},
+                                    2017: {'label': '2017'},
+                                    2018: {'label': '2018'},
+                                    2019: {'label': '2019'},
+                                    2020: {'label': '2020'}
+                                }
+                            ),
+                            html.Br(),
+                            html.Div(id='output-container-range-slider')
+                        ],
+                        style={'height': '600px', 'width': '1000px'}
+                    ),
+                    html.Div(
+                        className="twelve columns",
+                        children=[
+                            dcc.Markdown(d("""
+                            **Paper To Search**
+
+                            Input the paper to visualize.
+                            """)),
+                            dcc.Input(id="input1", type="text", placeholder="Title"),
+                            #dcc.Input(id="input2", type="text", placeholder="Goal Knowledge"),
+                            html.Div(id="output")
+                        ],
+                        style={'height': '300px'}
+                    )
+                ]
+            ),
+
+            ############################################middle graph component
+            html.Div(
+                className="eight columns",
+                children=[dcc.Graph(id="my-graph",
+                                    style={'margin-top':'180px'},
+                                    figure=network_graph(YEAR, ID))],
+            ),
+
+            #########################################right side two output component
+            html.Div(
+                className="two columns",
+                children=[
+                    html.Div(
+                        className='twelve columns',
+                        children=[
+                            dcc.Markdown(d("""
+                            **Hover Data**
+
+                            Mouse over values in the graph.
+                            """)),
+                            html.Pre(id='hover-data', style=styles['pre'])
+                        ],
+                        style={'height': '400px'}),
+
+                    html.Div(
+                        className='twelve columns',
+                        children=[
+                            dcc.Markdown(d("""
+                            **Click Data**
+
+                            Click on points in the graph.
+                            """)),
+                            html.Pre(id='click-data', style=styles['pre'])
+                        ],
+                        style={'height': '400px'})
+                ]
+            )
+        ]
+    )
+])
+
+###################################callback for left side components
+@app.callback(
+    dash.dependencies.Output('my-graph', 'figure'),
+    [dash.dependencies.Input('my-range-slider', 'value'), dash.dependencies.Input('input1', 'value')])
+def update_output(value,input1):
+    YEAR = value
+    ACCOUNT = input1
+    return network_graph(value, input1)
+    # to update the global variable of YEAR and ACCOUNT
+################################callback for right side components
+@app.callback(
+    dash.dependencies.Output('hover-data', 'children'),
+    [dash.dependencies.Input('my-graph', 'hoverData')])
+def display_hover_data(hoverData):
+    return json.dumps(hoverData, indent=2)
+
+
+@app.callback(
+    dash.dependencies.Output('click-data', 'children'),
+    [dash.dependencies.Input('my-graph', 'clickData')])
+def display_click_data(clickData):
+    return json.dumps(clickData, indent=2)
+
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
